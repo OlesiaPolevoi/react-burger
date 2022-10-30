627796ad175428ca3a857e638a4e9ab7f39f3f71
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-10-30 15:46:04 -0400

    updated components added scroll

--- src/components/BurgerConstructor/burger-constructor.jsx
+++ src/components/BurgerConstructor/burger-constructor.jsx
@@ -3,16 +3,26 @@ import "./burger-constructor.css";
 import {
   ConstructorElement,
   DragIcon,
+  Button,
+  CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
-// img
 export default function BurgerConstructor() {
   return (
     <section>
-      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-        <div className="burger-constructor-container">
-          <DragIcon type="primary" />
-
+      <div
+        className="burger-constructor__scroller"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          marginBottom: "40px",
+        }}
+      >
+        <div
+          className="burger-constructor-container"
+          style={{ marginLeft: "32px" }}
+        >
           <ConstructorElement
             type="top"
             isLocked={true}
@@ -24,24 +34,85 @@ export default function BurgerConstructor() {
           />
         </div>
 
-        <ConstructorElement
-          text="Краторная булка N-200i (верх)"
-          price={50}
-          thumbnail={"https://code.s3.yandex.net/react/code/mineral_rings.png"}
-        />
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (низ)"
-          price={200}
-          thumbnail={"https://code.s3.yandex.net/react/code/mineral_rings.png"}
-          // thumbnail={img}
-        />
+        <div className="burger-constructor-container">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i "
+            price={50}
+            thumbnail={
+              "https://code.s3.yandex.net/react/code/mineral_rings.png"
+            }
+          />
+        </div>
+
+        <div className="burger-constructor-container">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i "
+            price={50}
+            thumbnail={
+              "https://code.s3.yandex.net/react/code/mineral_rings.png"
+            }
+          />
+        </div>
+        <div className="burger-constructor-container">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i "
+            price={50}
+            thumbnail={
+              "https://code.s3.yandex.net/react/code/mineral_rings.png"
+            }
+          />
+        </div>
+        <div className="burger-constructor-container">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i "
+            price={50}
+            thumbnail={
+              "https://code.s3.yandex.net/react/code/mineral_rings.png"
+            }
+          />
+        </div>
+        <div className="burger-constructor-container">
+          <DragIcon type="primary" />
+          <ConstructorElement
+            text="Краторная булка N-200i "
+            price={50}
+            thumbnail={
+              "https://code.s3.yandex.net/react/code/mineral_rings.png"
+            }
+          />
+        </div>
+        <div
+          className="burger-constructor-container"
+          style={{ marginLeft: "32px" }}
+        >
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (низ)"
+            price={200}
+            thumbnail={
+              "https://code.s3.yandex.net/react/code/mineral_rings.png"
+            }
+          />
+        </div>
+      </div>
+
+      <div className="burger-constructor__total">
+        <div className="price">
+          <div className="burger-constructor__price-number price-number">
+            {610}
+          </div>
+          <CurrencyIcon type="primary" />
+        </div>
+
+        <Button type="primary" size="medium">
+          Оформить заказ
+        </Button>
       </div>
     </section>
   );
 }
-
-// () {
-//   return <section class="section"> HelloBurgerConstructor</section>;
-// }
--- src/components/BurgerIngredients/burger-ingredients.jsx
+++ src/components/BurgerIngredients/burger-ingredients.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Tab,
   CurrencyIcon,
+  Counter,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import "./burger-ingredients.css";
+import data from "/Users/olesiati/Desktop/react-burger/src/utils/data.js";
 
 export default function BurgerIngredients() {
   const [current, setCurrent] = React.useState("one");
 
   return (
-    <main>
+    <main className="scroller">
       <h1 className="main-header">Соберите бургер</h1>
 
       <div style={{ display: "flex" }} className="main-tab">
@@ -23,148 +25,66 @@ export default function BurgerIngredients() {
           Начинки
         </Tab>
       </div>
+      <IngredientsContainer
+        header="Булки"
+        cardsArr={data.filter((el) => {
+          return el.type === "bun";
+        })}
+      />
 
-      {/* <h2 className="secondary-header">Булки</h2>
-
-      <div className="container">
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/bun-02.png" />
-          <div className="price">
-            <div className="price-number">20</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Краторная булка N-200i</div>
-        </div>
-
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/bun-01.png" />
-          <div className="price">
-            <div className="price-number">20</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Флюоресцентная булка R2-D3</div>
-        </div>
-      </div> */}
-      <IngredientsContainer header="Булки" />
-      <h2 className="secondary-header">Соусы</h2>
-
-      <div className="container">
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/sauce-02.png" />
-          <div className="price">
-            <div className="price-number">30</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Соус Spicy-X</div>
-        </div>
-
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/sauce-04.png" />
-          <div className="price">
-            <div className="price-number">30</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Соус фирменный Space Sauce</div>
-        </div>
-
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/sauce-03.png" />
-          <div className="price">
-            <div className="price-number">30</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Соус традиционный галактический</div>
-        </div>
-
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/sauce-01.png" />
-          <div className="price">
-            <div className="price-number">30</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">
-            Соус с шипами Антарианского плоскоходца
-          </div>
-        </div>
-      </div>
-
-      <h2 className="secondary-header">Начинки</h2>
-
-      <div className="container">
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/sp_1.png" />
-          <div className="price">
-            <div className="price-number">80</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Плоды Фалленианского дерева</div>
-        </div>
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/mineral_rings.png" />
-          <div className="price">
-            <div className="price-number">80</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Хрустящие минеральные кольца</div>
-        </div>
-      </div>
+      <IngredientsContainer
+        header="Соусы"
+        cardsArr={data.filter((el) => {
+          return el.type === "sauce";
+        })}
+      />
+      <IngredientsContainer
+        header="Начинки"
+        cardsArr={data.filter((el) => {
+          return el.type === "main";
+        })}
+      />
     </main>
   );
 }
 
-function IngredientsContainer({ header }) {
+function IngredientsContainer({ header, cardsArr }) {
+  console.log(cardsArr);
   return (
     <>
       <h2 className="secondary-header">{header}</h2>
 
-      <div className="container">
-        {/* <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/bun-02.png" />
-          <div className="price">
-            <div className="price-number">20</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Краторная булка N-200i</div>
-        </div> */}
-        <Ingredient />
-
-        <div className="ingredient">
-          <img src="https://code.s3.yandex.net/react/code/bun-01.png" />
-          <div className="price">
-            <div className="price-number">20</div>
-            <CurrencyIcon type="primary" />
-          </div>
-
-          <div className="description">Флюоресцентная булка R2-D3</div>
-        </div>
+      <div className="burger-ingredients__container">
+        {cardsArr.map((el) => {
+          return (
+            <Ingredient name={el.name} price={el.price} image={el.image} />
+          );
+        })}
       </div>
     </>
   );
 }
 
-function Ingredient() {
+function Ingredient({ name, price, image }) {
+  const [count, setCount] = useState(0);
+  console.log(count);
+
+  const handleClick = () => {
+    setCount((prev) => {
+      return (prev += 1);
+    });
+  };
   return (
     <>
-      <div className="ingredient">
-        <img src="https://code.s3.yandex.net/react/code/bun-02.png" />
+      <div className="ingredient" onClick={handleClick}>
+        <img src={`${image}`} />
+        {count > 0 && <Counter count={count} size="default" />}
         <div className="price">
-          <div className="price-number">20</div>
+          <div className="price-number">{price}</div>
           <CurrencyIcon type="primary" />
         </div>
-
-        <div className="description">Краторная булка N-200i</div>
+        <div className="description">{name}</div>
       </div>
     </>
   );
 }
-
-// У компонента свой кастомизированный скроллбар. Подумайте над реализацией и возможным ограничением высоты блока, в том числе и на разных разрешениях экранов.
