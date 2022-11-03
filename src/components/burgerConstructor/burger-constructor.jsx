329de23ecbecf6712cd4405b6bc006a033e2f208
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-11-03 17:56:19 -0400

    updated burgerConstructor component to remove duplicating code

--- src/components/burgerConstructor/burger-constructor.jsx
+++ src/components/burgerConstructor/burger-constructor.jsx
@@ -7,7 +7,7 @@ import {
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import data from "../../utils/data.js";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 export default function BurgerConstructor() {
   return (
@@ -33,46 +33,93 @@ export default function BurgerConstructor() {
     </section>
   );
 }
- function ConstructorIngredient({dataArr}) {
-return (dataArr.map((el, index)=>{
-return(<>
-{index === 0 && <div key={el._id} className={burgerConstructor.container}   className={burgerConstructor.margin}>
-{index===0 && <ConstructorElement 
-type="top" 
-isLocked={true}    
-text={el.name}
-price={el.price}
-thumbnail={el.image}/>}
-</div>
-}
+function ConstructorIngredient({dataArr}) {
+  return (dataArr.map((el, index)=>{
+    return (
+      <div key={el._id} className={`${burgerConstructor.container} ${index === 0 || 
+        index ===dataArr.length -1 ? burgerConstructor.margin : ""}`}  
+      >
+        {
+          index !== 0 && index !== dataArr.length -1 && <DragIcon type="primary" />
+        }
+        <ConstructorElement 
+          type={index === 0 ? "top" : index === dataArr.length - 1 ? "bottom" : undefined} 
+          isLocked={index === 0 || index === dataArr.length - 1}    
+          text={el.name}
+          price={el.price}
+          thumbnail={el.image}
+        />
+      </div>
+    )
 
-{ index !== 0 && index !==dataArr.length-1 && <div key={el._id} className={burgerConstructor.container}> 
-    {index !== 0 && index !==dataArr.length-1 && <DragIcon type="primary"/>}
-    {index !== 0 && index !==dataArr.length-1 && <ConstructorElement
-    text={el.name}
-    price={el.price}
-    thumbnail={el.image}
-    /> }
-  </div>}
+    // if(index === 0) {
+    //   return (
+    //     <div key={el._id} className={burgerConstructor.container}   className={burgerConstructor.margin}>
+    //        <ConstructorElement 
+    //       type="top" 
+    //       isLocked={true}    
+    //       text={el.name}
+    //       price={el.price}
+    //       thumbnail={el.image}/>
+    //     </div>
+    //   )
+    // }
+    // if(index !== 0 && index !== dataArr.length-1){
+    //   return(
+    //     <div key={el._id} className={burgerConstructor.container}>
+    //       <DragIcon type="primary" />
+    //       <ConstructorElement
+    //         text={el.name}
+    //         price={el.price}
+    //         thumbnail={el.image}
+    //       />
+    //     </div>
+    //   )
+    // }
+    // if(index === dataArr.length-1){
+    //   return(
+    //     <div key={el._id} className={burgerConstructor.container}   className={burgerConstructor.margin}>
+    //       <ConstructorElement 
+    //         type="bottom" 
+    //         isLocked={true}    
+    //         text={el.name}
+    //         price={el.price}
+    //         thumbnail={el.image}
+    //        />
+    //     </div>
+    //   )
+    // }
 
-{index === dataArr.length-1 && <div key={el._id} className={burgerConstructor.container}   className={burgerConstructor.margin}>
-{index===dataArr.length-1 && <ConstructorElement 
-type="bottom" 
-isLocked={true}    
-text={el.name}
-price={el.price}
-thumbnail={el.image}/>}
-</div>
-}
-</>  
-  )}))
+  }
+))}  
+// { index !== 0 && index !==dataArr.length-1 && < key={el._id} className={burgerConstructor.container}> 
+//     {index !== 0 && index !==dataArr.length-1 && <DragIcon type="primary"/>}
+//     {index !== 0 && index !==dataArr.length-1 && <ConstructorElement
+//     text={el.name}
+//     price={el.price}
+//     thumbnail={el.image}
+//     /> }
+//   }
+
+// {index === dataArr.length-1 && <div key={el._id} className={burgerConstructor.container}   className={burgerConstructor.margin}>
+// {index===dataArr.length-1 && 
+// <ConstructorElement 
+// type="bottom" 
+// isLocked={true}    
+// text={el.name}
+// price={el.price}
+// thumbnail={el.image}/>}
+// </div>
+// }
+// </>  
+// )}))
     
-}
+// }
 
-Button.propTypes = {
-  type: PropTypes.string.isRequired,
-  size: PropTypes.string.isRequired
-};
+// Button.propTypes = {
+//   type: PropTypes.string.isRequired,
+//   size: PropTypes.string.isRequired
+// };
 
 
 
