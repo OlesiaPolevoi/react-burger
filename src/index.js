45b8450bd097b9189d78841aea770aa18185a479
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-11-09 09:03:11 -0500

    changed file name to index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import "@ya.praktikum/react-developer-burger-ui-components";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
